27097a35380a32c6852eb355d087ba12b9797e5a
Steven Webber <webber.steven@gmail.com>
2025-06-30 17:57:01 +0200

    Fixed the test logic - not quarkus start issue

--- backend/src/test/java/za/co/sww/rwars/CucumberTest.java
+++ backend/src/test/java/za/co/sww/rwars/CucumberTest.java
@@ -1,6 +1,6 @@
 package za.co.sww.rwars;
 
-import io.quarkus.test.junit.QuarkusTest;
+import io.quarkiverse.cucumber.CucumberQuarkusTest;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -11,12 +11,12 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 /**
  * Test runner for Cucumber tests.
+ * Using CucumberQuarkusTest to bootstrap Cucumber in the Quarkus project.
  */
-@Suite
-@QuarkusTest
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "za.co.sww.rwars.steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-public class CucumberTest {
+//@Suite
+//@IncludeEngines("cucumber")
+//@SelectClasspathResource("features")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "za.co.sww.rwars.steps")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class CucumberTest extends CucumberQuarkusTest {
 }
