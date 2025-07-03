cae8bb80cc1ffa72aa6e64138864f444ab5a3d7a
Steven Webber <webber.steven@gmail.com>
2025-07-03 17:19:14 +0200

    Clearer feature definition to ensure unique robot id

--- backend/src/main/java/za/co/sww/rwars/backend/model/Robot.java
+++ backend/src/main/java/za/co/sww/rwars/backend/model/Robot.java
@@ -1,22 +1,36 @@
 package za.co.sww.rwars.backend.model;
 
+import java.util.UUID;
+
 /**
  * Represents a robot in the battle.
  */
 public class Robot {
+    private String id;
     private String name;
     private String battleId;
 
     public Robot() {
+        this.id = UUID.randomUUID().toString();
     }
 
     public Robot(String name) {
+        this.id = UUID.randomUUID().toString();
         this.name = name;
     }
 
     public Robot(String name, String battleId) {
+        this.id = UUID.randomUUID().toString();
         this.name = name;
         this.battleId = battleId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
